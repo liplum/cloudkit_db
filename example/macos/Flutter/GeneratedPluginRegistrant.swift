02d89c4fa95c57bbfc57f9f4b8cba2cd237a767e
Liplum <Li_plum@outlook.com>
2023-11-21 21:42:09 +0800

    build iOS and macOS

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import cloudkit_db
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  CloudkitDbPlugin.register(with: registry.registrar(forPlugin: "CloudkitDbPlugin"))
+}
